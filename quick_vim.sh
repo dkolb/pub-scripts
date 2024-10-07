74480170faf8d76936c0de2345738939f9916ef4
David Kolb <git@dkub.dev>
2024-10-07 13:56:46 -0400

    better keybinds

--- quick_vim.sh
+++ quick_vim.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p $HOME/.vim/pack/tpope/start
-cd $HOME/.vim/pack/tpope/start
+mkdir -p "$HOME/.vim/pack/tpope/start"
+cd "$HOME/.vim/pack/tpope/start"
 
 # All those netrw keybinds and settings you can't live without
 git clone 'https://github.com/tpope/vim-vinegar.git'
@@ -21,6 +21,38 @@ vim -u NONE -c "helptags sleuth/doc" -c q
 git clone 'https://tpope.io/vim/surround.git'
 vim -u NONE -c "helptags surround/doc" -c q
 
-mkdir -p $HOME/.vim/pack/sheerun/start
-cd $HOME/.vim/pack/sheerun/start
+mkdir -p "$HOME/.vim/pack/sheerun/start"
+cd "$HOME/.vim/pack/sheerun/start"
 git clone 'https://github.com/sheerun/vim-polyglot.git'
+
+mkdir -p "$HOME/.vim/plugin"
+cat >"$HOME/.vim/plugin/keybinds.vim" <<EOF
+" ############
+" # KEYBINDS #
+" ############
+let mapleader = ";"
+
+"Window navigation
+nnoremap <Leader>wh <C-w>h
+nnoremap <Leader>wj <C-w>j
+nnoremap <Leader>wk <C-w>k
+nnoremap <Leader>wl <C-w>l
+nnoremap <Leader>ws <C-w>s
+nnoremap <Leader>wv <C-w>v
+nnoremap <Leader>w= <C-w>=
+
+"Tab navigation
+nnoremap <Leader>tn <Cmd>tabnext<CR>
+nnoremap <Leader>tp <Cmd>tabprevious<CR>
+nnoremap <Leader>tt <Cmd>tabnew<CR>
+
+"Quicklist navigation
+nnoremap <Leader>qo <Cmd>copen<CR>
+nnoremap <Leader>qc <Cmd>cclose<CR>
+nnoremap <Leader>qn <Cmd>cnext<CR>
+nnoremap <Leader>qp <Cmd>cprevious<CR>
+nnoremap <Leader>qd :cdo<Space>
+
+"No highlight
+nnoremap <Leader>nh <Cmd>nohl<CR>
+EOF
